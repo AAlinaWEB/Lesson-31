120dbde27cb73ba3d813b9dcc2e5628adfe960f5
Anonimca <tova304050@gmail.com>
2022-02-02 16:31:37 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,104 @@
+// let a = -1
+// if (a>0) {
+//     b=a+1
+//    alert(b) 
+// } else{
+//     alert(a)
+// }
+
+// let a = 0
+// if (a>0) {
+//     b=a+1
+//     alert(b)
+// } else {
+//     b=a-2
+//     alert(b)
+// }
+
+// let a = 0
+// if (a>0) {
+//     b=a+1
+//     alert(b)
+// } else if (a<0) {
+//     b=a-2
+//     alert(b)
+// } else {
+//     b=10
+//     alert(b)
+// }
+
+// let a = 1
+// let b = 0
+// if (a>b) {
+//     alert(a)
+// } else {
+//     alert(b)
+// }
+
+
+
+
+// let y = prompt ('Введите число?')
+// let x = Number(y)
+// if (x>0) {
+//     x +=1 
+//     console.log(x)
+// }
+// else {
+//     console.log (x)
+// }
+
+// let a = prompt ('Введите число?')
+// let b = Number(a)
+// if (b>0) {
+//     b+=1
+//     console.log(b)
+// }
+// else {
+//     b-=2
+//     console.log(b)
+// }
+
+// let a = prompt ('Введите чило?')
+// let b = Number(a)
+// if (b>0) {
+//     b+=1
+//     console.log(b)
+// } 
+// else if (b<0) {
+//     b-=2
+//     console.log(b)
+// }
+// else {
+//     console.log(b)
+// }
+
+// let a = prompt ('Введите число?')
+// let c = prompt ('Введите число?')
+// let b = Number(a)
+// let d = Number(c)
+// if (b>d) {
+//     console.log(b)
+// }
+// else{
+//     console.log(d)
+// }
+
+
+// let A = prompt ('Введите А')
+// let a = Number (A)
+// if (a>0) {
+//     console.log (true)
+// }
+// else {
+//     console.log (false)
+// }
+
+let a = prompt ('Введите число')
+let A = Number (a)
+if (A%2==0) {
+    console.log ('Число - четное')
+}
+else {
+    console.log ('Число - нечетное')
+}
